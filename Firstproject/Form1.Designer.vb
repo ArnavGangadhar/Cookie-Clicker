d24aac058941f07cb41bb4ad534191bddc91f31a
Arnav <Arnav@KARNEY>
2023-03-11 12:14:46 -0500

    Add project files.

---
+++ Firstproject/Form1.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Cookie_Value = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Passivecookietimer = New System.Windows.Forms.Timer(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lbl_cost_click = New System.Windows.Forms.Label()
+        Me.lbl_cost_wide = New System.Windows.Forms.Label()
+        Me.Round_button1 = New Firstproject.Round_button()
+        Me.SuspendLayout()
+        '
+        'Cookie_Value
+        '
+        Me.Cookie_Value.Location = New System.Drawing.Point(0, 0)
+        Me.Cookie_Value.Name = "Cookie_Value"
+        Me.Cookie_Value.Size = New System.Drawing.Size(100, 23)
+        Me.Cookie_Value.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(0, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(0, 0)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 0
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(0, 0)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(100, 100)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 23)
+        Me.Label1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(0, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 23)
+        Me.Label2.TabIndex = 0
+        '
+        'lbl_cost_click
+        '
+        Me.lbl_cost_click.Location = New System.Drawing.Point(0, 0)
+        Me.lbl_cost_click.Name = "lbl_cost_click"
+        Me.lbl_cost_click.Size = New System.Drawing.Size(100, 23)
+        Me.lbl_cost_click.TabIndex = 0
+        '
+        'lbl_cost_wide
+        '
+        Me.lbl_cost_wide.Location = New System.Drawing.Point(0, 0)
+        Me.lbl_cost_wide.Name = "lbl_cost_wide"
+        Me.lbl_cost_wide.Size = New System.Drawing.Size(100, 23)
+        Me.lbl_cost_wide.TabIndex = 0
+        '
+        'Round_button1
+        '
+        Me.Round_button1.Location = New System.Drawing.Point(274, 140)
+        Me.Round_button1.Name = "Round_button1"
+        Me.Round_button1.Size = New System.Drawing.Size(137, 138)
+        Me.Round_button1.TabIndex = 0
+        Me.Round_button1.Text = "Round_button1"
+        Me.Round_button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.ClientSize = New System.Drawing.Size(699, 449)
+        Me.Controls.Add(Me.Round_button1)
+        Me.Name = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Cookie_Value As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Passivecookietimer As Timer
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lbl_cost_click As Label
+    Friend WithEvents lbl_cost_wide As Label
+    Friend WithEvents grandma As Button
+    Friend WithEvents grandma_cost As Label
+    Friend WithEvents Round_button1 As Round_button
+End Class
---
+++ Firstproject/Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+    Public totalCookies, ClickValue, ClickLevel As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs)
+        totalCookies = 0
+        ClickValue = 1
+        ClickLevel = 1
+        set_Timer(100)
+        Passivecookietimer.Start()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+    Private Sub cookie_click()
+        totalCookies = totalCookies + ClickValue
+        Cookie_Value.Text = "value" + totalCookies.ToString
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        cookie_click()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs)
+        totalCookies = totalCookies + 10
+        Cookie_Value.Text = "Value: " + totalCookies.ToString
+    End Sub
+
+    Private Sub Form1_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub set_Timer(interval As Integer)
+        Passivecookietimer.Interval = interval
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs)
+        Dim upgradeCost As Integer
+        upgradeCost = ClickLevel * 2
+
+        If totalCookies >= upgradeCost Then
+            totalCookies = totalCookies - upgradeCost
+
+            ClickLevel = ClickLevel + 1
+            ClickValue = (ClickLevel - 1) * 5
+
+            upgradeCost = ClickLevel * 2
+            lbl_cost_click.Text = "LvL: " + ClickLevel.ToString
+            lbl_cost_wide.Text = "Price: " + upgradeCost.ToString
+            Button1.Text = "Click to increase cookies by " + ClickValue.ToString
+        End If
+    End Sub
+End Class
+
